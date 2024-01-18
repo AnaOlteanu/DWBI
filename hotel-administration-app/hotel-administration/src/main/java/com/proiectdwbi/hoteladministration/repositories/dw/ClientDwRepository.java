86ab18747c4999e56675d4f1a242ac1948404f27
Ana Olteanu <49120666+AnaOlteanu@users.noreply.github.com>
2024-01-18 12:52:05 +0200

    Delete hotel-administration-app/hotel-administration directory

--- hotel-administration-app/hotel-administration/src/main/java/com/proiectdwbi/hoteladministration/repositories/dw/ClientDwRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proiectdwbi.hoteladministration.repositories.dw;
-
-import com.proiectdwbi.hoteladministration.models.dw.ClientDw;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientDwRepository extends JpaRepository<ClientDw, Long> {
-}
--- hotel-administration-app/hotel-administration/src/main/java/com/proiectdwbi/hoteladministration/repositories/oltp/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proiectdwbi.hoteladministration.repositories.oltp;
-
-import com.proiectdwbi.hoteladministration.models.oltp.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
